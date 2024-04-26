20d89614be0e1903b98507b567c8112419390133
AlvaroNajera <143565768+AlvaroNajera@users.noreply.github.com>
2024-04-26 16:32:37 -0500

    TiendaOnline
    Proyecto

---
+++ VirtualStopHope/src/main/java/Users/TesUser.java
@@ -0,0 +1,19 @@
+package Users;
+
+public class TesUser {
+
+
+    public static void main(String[] args) {
+        User user1 = new User();
+
+        user1.setId(1);
+        System.out.println(user1.getId());
+
+
+
+
+
+    }
+
+
+}
---
+++ VirtualStopHope/src/main/java/Users/User.java
@@ -0,0 +1,36 @@
+package Users;
+
+public class User {
+
+    // Atributos encapsulados con el modificador private
+
+    private int id;
+
+    protected String name;
+
+    private String lastName;
+
+    private String phone;
+
+    private String email;
+
+    private String password;
+
+    // Constructor de la clase
+
+    public User(){
+
+    }
+//Getter and Setter
+
+    public int getId(){
+
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+}
+
+
